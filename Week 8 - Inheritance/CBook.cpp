049726204fe8ea83471221f5b84b8f578424aa69
sZew2000 <sewerynmorys@outlook.com>
2026-01-30 22:34:21 +0000

    added Getter + Setter

---
+++ Week 8 - Inheritance/CBook.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "CBook.hpp"
+
+using namespace std;
+
+CBook::CBook(string title, string author)
+{
+
+	mTitle = title; 
+	mAuthor = author;
+
+}
---
+++ Week 8 - Inheritance/CBook.hpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+using namespace std;
+
+
+class CBook
+{
+
+private:
+
+	string mTitle;
+	string mAuthor;
+
+public:
+
+	CBook(string title, string author);
+
+
+
+
+};
+
